166eddc715a6d452fe68d1f38dfa571cdb2721b1
Nolan Schuller <naschuller@shaw.ca>
2026-01-15 11:03:05 -0700

    created lesson 3

---
+++ Lesson03_Loops/Lesson03_Loops/LoopGame.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lesson03_Loops;
+
+public class LoopGame : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private Texture2D _pixel;
+
+    private Vector2 _position, _dimensions;
+
+    private int _count;
+
+    private float _spacing;
+
+    public LoopGame()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        _position = new Vector2(50, 200);
+        _dimensions = new Vector2(60, 40);
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _pixel = new Texture2D(GraphicsDevice, 1, 1);
+        _pixel.SetData(new [] {Color.White});
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin();
+        Rectangle rect = new Rectangle((int)_position.X, (int)_position.Y, (int)_dimensions.X, (int)_dimensions.Y);
+        _spriteBatch.Draw(_pixel, rect, Color.Aquamarine);
+        _spriteBatch.End();
+        base.Draw(gameTime);
+    }
+}
