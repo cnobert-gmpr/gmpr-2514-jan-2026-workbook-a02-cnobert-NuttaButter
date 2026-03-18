0147ef94868ce38c1480662fee6d7f0ea2911eb3
Nolan Schuller <naschuller@shaw.ca>
2026-03-18 09:27:12 -0600

    end day 19

--- Lesson08_MosquitoAttack/Lesson08_MosquitoAttack/Cannon.cs
+++ Lesson08_MosquitoAttack/Lesson08_MosquitoAttack/Cannon.cs
@@ -6,14 +6,16 @@ namespace Lesson08_MosquitoAttack;
 
 public class Cannon
 {
+    private const int _NumCannonBalls = 10;
     private SimpleAnimation _animation;
     private Vector2 _position, _direction;
     private Point _dimensions;
     private float _speed;
 
-    private CannonBall _cBall;
-
     private Rectangle _gameBoundingBox;
+
+    private CannonBall[] _cBalls;
+
     internal Vector2 Direction
     {
         set
@@ -22,12 +24,22 @@ public class Cannon
             value.Y = 0;
             _direction = value;
             if(_direction.X < 0)
-            {
-              _animation.Reverse = true;  
-            } else
-            {
+                _animation.Reverse = true;
+            else
                 _animation.Reverse = false;
-            }
+        }
+    }
+
+    internal Rectangle BoundingBox
+    {
+        get
+        {
+            return new Rectangle(
+                (int)_position.X,
+                (int)_position.Y,
+                (int)_animation.FrameDimensions.X,
+                (int)_animation.FrameDimensions.Y
+            );
         }
     }
 
@@ -36,44 +48,64 @@ public class Cannon
         _position = position;
         _speed = speed;
         _gameBoundingBox = gameBoundingBox;
-        _cBall = new CannonBall();
-        _cBall.Initialize(_position.X, _gameBoundingBox);
-    }
+        _cBalls = new CannonBall[_NumCannonBalls];
+        for(int c=0; c<_NumCannonBalls; c++)
+        {
+            _cBalls[c] = new CannonBall();
+            _cBalls[c].Initialize(50, _gameBoundingBox);
+        }
+        
 
+    }
     internal void LoadContent(ContentManager content)
     {
         Texture2D texture = content.Load<Texture2D>("Cannon");
-        _dimensions = new Point(texture.Width/4, texture.Height);
+        _dimensions = new Point(texture.Width / 4, texture.Height);
         _animation = new SimpleAnimation(texture, _dimensions.X, _dimensions.Y, 4, 2);
 
-        _cBall.LoadContent(content);
+        foreach(CannonBall c in _cBalls)
+        {
+            c.LoadContent(content); 
+        }
+        
     }
-
     internal void Update(GameTime gameTime)
     {
         float dt = (float) gameTime.ElapsedGameTime.TotalSeconds;
         _position += _direction * _speed * dt;
-
         if(_direction != Vector2.Zero)
+            _animation.Update(gameTime);
+
+        foreach(CannonBall c in _cBalls)
         {
-           _animation.Update(gameTime); 
+            c.Update(gameTime);
         }
-
-        _cBall.Update(gameTime);
+        
     }
-
     internal void Draw(SpriteBatch spriteBatch)
     {
         if(_animation != null)
-        {
             _animation.Draw(spriteBatch, _position, SpriteEffects.None);
+        
+        foreach(CannonBall c in _cBalls)
+        {
+            c.Draw(spriteBatch); 
         }
-
-        _cBall.Draw(spriteBatch);
+        
     }
-
     internal void Shoot()
     {
-        _cBall.Shoot(_position, new Vector2(0, -1));
+        foreach(CannonBall c in _cBalls)
+        {
+            if (c.Launchable)
+            {
+                float cannonBallPositionY = BoundingBox.Top - c.BoundingBox.Height;
+                float cannonBallPositionX = BoundingBox.Center.X - c.BoundingBox.Width / 2;
+                Vector2 cannonBallPosition = new Vector2(cannonBallPositionX, cannonBallPositionY);
+                c.Shoot(cannonBallPosition, new Vector2(0, -1));
+                return;
+            }
+        }
+        
     }
 }
--- Lesson08_MosquitoAttack/Lesson08_MosquitoAttack/CannonBall.cs
+++ Lesson08_MosquitoAttack/Lesson08_MosquitoAttack/CannonBall.cs
@@ -10,8 +10,10 @@ public class CannonBall
     private Vector2 _position;
     private Vector2 _direction;
     private float _speed;
+
     private Rectangle _gameBoundingBox;
-    private enum State {Flying, NotFlying};
+
+    private enum State { Flying, NotFlying}
     private State _state = State.NotFlying;
 
     internal Rectangle BoundingBox
@@ -19,36 +21,40 @@ public class CannonBall
         get => new Rectangle((int)_position.X, (int)_position.Y, _texture.Width, _texture.Height);
     }
 
+    internal bool Launchable { get => _state == State.NotFlying; }
+
     internal void Initialize(float speed, Rectangle gameBoundingBox)
     {
         _position = Vector2.Zero;
-        _speed = speed;
         _direction = Vector2.Zero;
+        _speed = speed;
         _gameBoundingBox = gameBoundingBox;
     }
-
     internal void LoadContent(ContentManager content)
     {
         _texture = content.Load<Texture2D>("CannonBall");
     }
-
     internal void Update(GameTime gameTime)
     {
         float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-        switch (_state)
+        switch(_state)
         {
-            case State.Flying:     
+            case State.Flying:
                 _position += _direction * _speed * dt;
+                if(!BoundingBox.Intersects(_gameBoundingBox))
+                {
+                    //I'm not on screen anymore
+                    _state = State.NotFlying;
+                }
                 break;
             case State.NotFlying:
                 break;
         }
     }
-
     internal void Draw(SpriteBatch spriteBatch)
     {
-        switch (_state)
+        switch(_state)
         {
             case State.Flying:
                 spriteBatch.Draw(_texture, _position, Color.White);
@@ -56,12 +62,15 @@ public class CannonBall
             case State.NotFlying:
                 break;
         }
+        
     }
-
     internal void Shoot(Vector2 position, Vector2 direction)
     {
-        _position = position;
-        _direction = direction;
-        _state = State.Flying;
+        if(_state == State.NotFlying)
+        {
+            _position = position;
+            _direction = direction;
+            _state = State.Flying;
+        }
     }
 }
